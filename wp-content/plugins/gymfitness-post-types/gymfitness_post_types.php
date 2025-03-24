a0ba26203442d833e4f0c8bd4a0df8e4dadf86de
Gerardo <gerardo.dev21@hotmail.com>
2025-03-23 23:15:44 -0600

    Implementando mi primer plugin de WordPress.

---
+++ wp-content/plugins/gymfitness-post-types/gymfitness_post_types.php
@@ -0,0 +1,10 @@
+<?php
+/*
+    Plugin Name: Gym Fitness - Post Types
+    Plugin URI: http://twitter.com/codigoconjuan
+    Description: Añade Post Types al sitio Gym Fitness
+    Version: 1.0.0
+    Author: Gerardo Macías
+    Author URI: http://twitter.com/codigoconjuan
+    Text Domain: gymfitness
+*/
